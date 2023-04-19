e7190664785bf58151ba641351fb38d5de691863
Oleksandr Larin <sashalarin202@gmail.com>
2023-04-19 23:39:36 +0100

    add references dialog

--- web-auth/src/app/components/dialog/dialog.component.ts
+++ web-auth/src/app/components/dialog/dialog.component.ts
@@ -10,11 +10,18 @@ export class DialogComponent implements OnInit{
   hide: boolean = true;
   signInform: FormGroup;
   constructor(private fb: FormBuilder) { }
-
+  
   ngOnInit(){
     this.signInform = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
+}
+
+export interface DialogInterface {
+  cancelButtonLabel: string;
+  confirmButtonLabel: string;
+  dialogHeader: string;
+  dialogContent: string;
 }
--- web-auth/src/app/components/header/header.component.ts
+++ web-auth/src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogComponent } from '../dialog/dialog.component';
+import { DialogComponent, DialogInterface } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +11,17 @@ export class HeaderComponent {
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
-    this.dialog.open(DialogComponent);
+
+    const dialogInterface: DialogInterface = {
+      dialogHeader: 'I am created by Reusable dialog',
+      dialogContent: 'I am second dialog',
+      cancelButtonLabel: 'Cancel',
+      confirmButtonLabel: 'Submit',
+    };
+
+    this.dialog.open(DialogComponent,{
+      width: '300px',
+      data: dialogInterface,
+    });
   }
 }
